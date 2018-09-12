76745b6da402980f08828b94b1c0f9fd912bc95e
RakhiDemsi <RakhiDemsi@LAPTOP-LNN259SC>
2018-09-11 19:34:30 -0700

    Tambah Input

--- src/HitungFibonacci/HitungFibonacci.java
+++ src/HitungFibonacci/HitungFibonacci.java
@@ -7,10 +7,20 @@ public class HitungFibonacci {
         
         System.out.println("\nHitung Fibonacci");
         System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ...dst.\n");
-    }    
+    }
+    private static int tampilInput()
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Bilangan Ke-: ");
+        int n = scanner.nextInt();
+        
+        return n;
+    }
     public static void main(String[] args) 
     {
         String identitas = "Muhammad Zidan Rakhi Ismed / X - RPL 3 / 26";
         tampilJudul(identitas);
+        int n = tampilInput();
     }
 }
