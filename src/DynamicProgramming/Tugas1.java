3b7d998348af25507dbc08b59c0e944d1e4f0b94
RakhiDemsi <RakhiDemsi@LAPTOP-LNN259SC>
2018-09-14 08:58:35 -0700

    Tugas1 - Method InputUser

---
+++ src/DynamicProgramming/Tugas1.java
@@ -0,0 +1,14 @@
+package DynamicProgramming;
+import java.util.Scanner;
+public class Tugas1 {        
+    public static int InputUser()
+    {
+        Scanner baca = new Scanner(System.in);
+        System.out.print("Jumlah Uang: Rp");
+        int uang = baca.nextInt();
+        
+        return uang;
+    }
+    
+    
+}
