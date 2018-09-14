d0880c4ba8a552137cd25cc43602cdd077e73e55
RakhiDemsi <RakhiDemsi@LAPTOP-LNN259SC>
2018-09-14 09:06:58 -0700

    Tugas2 - Tambah Method Proses

--- src/DynamicProgramming/Tugas2.java
+++ src/DynamicProgramming/Tugas2.java
@@ -10,5 +10,21 @@ public class Tugas2 {
         return berat;
     }
     
-    
+    public static void Proses(int berat, int[] Berat, int[] Harga)
+    {
+        int jumlah = 0;
+        for (int i = 0;i < Berat.length; i++)
+        {
+            int a = 0;
+            while(berat >= Berat[i])
+            {
+                berat = berat - Berat[i];
+                a++;
+            }            
+            int total = a * Harga[i];
+            System.out.println(Berat[i] + "Kg Sebanyak " + a + " Barang,Dengan Harga Rp " + total);
+            jumlah = jumlah + total;
+        }
+        System.out.println("\nTotal Bayaran : Rp " + jumlah);
+    }
 }
