9bcbaba3fef2e71207423873a5ac020de59ad362
RakhiDemsi <RakhiDemsi@LAPTOP-LNN259SC>
2018-09-14 09:04:53 -0700

    Tugas2 - Method Input

---
+++ src/DynamicProgramming/Tugas2.java
@@ -0,0 +1,14 @@
+package DynamicProgramming;
+import java.util.Scanner;
+public class Tugas2 {
+    public static int Input()
+    {
+        Scanner baca = new Scanner(System.in);
+        System.out.print("Berat Barang yang Dibawa: ");
+        int berat = baca.nextInt();
+        
+        return berat;
+    }
+    
+    
+}
